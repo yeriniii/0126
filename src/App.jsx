acabb66aa84dbf0d27d4a201cbf93e45ab9b099e
yeriniii <yerini000303@naver.com>
2024-01-26 17:01:53 +0900

    ex3

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,25 @@ function App() {
 
   // TODO: filterByAge 함수를 작성하세요. 이 함수는 최소 나이를 매개변수로 받아 해당 나이 이상인 학생들로 필터링해야 합니다.
   const filterByAge = (minAge) => {
+    const filteredByAge = students.filter((student) => {
+      return student.age >= minAge;
+    });
+    setFilteredStudents(filteredByAge);
     // 여기에 코드를 작성하세요.
   };
 
   // TODO: filterByGrade 함수를 작성하세요. 이 함수는 특정 학점을 매개변수로 받아 해당 학점의 학생들로 필터링해야 합니다.
   const filterByGrade = (grade) => {
+    const filteredByGrade = students.filter((student) => {
+      return student.grade === grade;
+    });
+    setFilteredStudents(filteredByGrade);
     // 여기에 코드를 작성하세요.
   };
 
   // TODO: resetFilter 함수를 작성하세요. 이 함수는 필터를 초기화하여 모든 학생들을 표시해야 합니다.
   const resetFilter = () => {
+    setFilteredStudents(students);
     // 여기에 코드를 작성하세요.
   };
 
